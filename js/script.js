6af88d32284a008ba5b24512e523ed16447a7da6
Arifblogger77 <Arifblogger77@gmail.com>
2023-07-24 16:23:38 +0700

    menambahkan fungsi memuat data ketika todo apps dibuka

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault();
     addTodo();
   });
+
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 function generateId() {
@@ -38,6 +42,19 @@ function saveData() {
     localStorage.setItem(STORAGE_KEY, parsed);
     document.dispatchEvent(new Event(SAVED_EVENT));
   }
+}
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
 function addTodo() {
